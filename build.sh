fde5d385452157f2bc9803a253a8836b636ba917
dantesieg <bahadirb@me.com>
2024-06-28 02:55:20 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
-
+KERNEL="$(rpm -q kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 
 ### Install packages
 
@@ -19,5 +19,25 @@ RELEASE="$(rpm -E %fedora)"
 # rpm-ostree install vlc
 
 #### Example for enabling a System Unit File
+
+# always disable cisco-open264 repo
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-cisco-openh264.repo
+
+#### INSTALL
+# inspect to see what RPMS we copied in
+find /tmp/rpms/
+
+rpm-ostree install /tmp/rpms/ublue-os-ucore-addons-*.rpm
+
+    curl -L https://negativo17.org/repos/fedora-nvidia.repo -o /etc/yum.repos.d/fedora-nvidia.repo
+
+    rpm-ostree install /tmp/rpms/nvidia/ublue-os-ucore-nvidia-*.rpm
+    sed -i '0,/enabled=0/{s/enabled=0/enabled=1/}' /etc/yum.repos.d/nvidia-container-toolkit.repo
+
+    rpm-ostree install \
+        /tmp/rpms/nvidia/kmod-nvidia-*.rpm \
+        nvidia-driver \
+        nvidia-driver-libs.i686 \
+        nvidia-settings
 
 systemctl enable podman.socket
