cdc1b95253d7cb7fbc8a5fb1367de5bdafa5e91b
dantesieg <bahadirb@me.com>
2024-06-28 03:39:12 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -20,12 +20,42 @@ KERNEL="$(rpm -q kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 
 #### Example for enabling a System Unit File
 
-# always disable cisco-open264 repo
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-cisco-openh264.repo
+RELEASE="$(rpm -E '%fedora.%_arch')"
+
+cd /tmp
+
+### BUILD nvidia
+
+dnf install -y \
+    akmod-nvidia*:*.fc${RELEASE}
+
+# Either successfully build and install the kernel modules, or fail early with debug output
+rpm -qa |grep nvidia
+KERNEL_VERSION="$(rpm -q kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
+NVIDIA_AKMOD_VERSION="$(basename "$(rpm -q "akmod-nvidia" --queryformat '%{VERSION}-%{RELEASE}')" ".fc${RELEASE%%.*}")"
+
+
+akmods --force --kernels "${KERNEL_VERSION}" --kmod "nvidia"
+
+modinfo /usr/lib/modules/${KERNEL_VERSION}/extra/nvidia/nvidia{,-drm,-modeset,-peermem,-uvm}.ko.xz > /dev/null || \
+(cat /var/cache/akmods/nvidia/${NVIDIA_AKMOD_VERSION}-for-${KERNEL_VERSION}.failed.log && exit 1)
+
+# create a directory for later copying of resulting nvidia specific artifacts
+mkdir -p /var/cache/rpms/kmods/nvidia
+
+cat <<EOF > /var/cache/rpms/kmods/nvidia/nvidia-vars
+KERNEL_VERSION=${KERNEL_VERSION}
+RELEASE=${RELEASE}
+NVIDIA_AKMOD_VERSION=${NVIDIA_AKMOD_VERSION}
+EOF
+
+mv /var/cache/akmods/nvidia/*.rpm \
+   /tmp/rpms/nvidia/
 
     curl -L https://negativo17.org/repos/fedora-nvidia.repo -o /etc/yum.repos.d/fedora-nvidia.repo
 
     rpm-ostree install \
+        /tmp/rpms/nvidia/kmod-nvidia-*.rpm \
         nvidia-driver \
         nvidia-driver-libs.i686 \
         nvidia-settings
