d04ae1c2bf1b3fa9a43ffba1ba3f58d35f0e9c9d
dantesieg <bahadirb@me.com>
2024-06-28 03:20:42 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -23,18 +23,9 @@ KERNEL="$(rpm -q kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 # always disable cisco-open264 repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-cisco-openh264.repo
 
-#### INSTALL
-# inspect to see what RPMS we copied in
-find /tmp/rpms/
-
-rpm-ostree install /tmp/rpms/ublue-os-ucore-addons-*.rpm
-
     curl -L https://negativo17.org/repos/fedora-nvidia.repo -o /etc/yum.repos.d/fedora-nvidia.repo
 
-    rpm-ostree install /tmp/rpms/nvidia/ublue-os-ucore-nvidia-*.rpm
-
     rpm-ostree install \
-        /tmp/rpms/nvidia/kmod-nvidia-*.rpm \
         nvidia-driver \
         nvidia-driver-libs.i686 \
         nvidia-settings
