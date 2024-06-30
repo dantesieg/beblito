a0061901302d250aaded512024b3660741d89fd5
dantesieg <bahadirb@me.com>
2024-06-30 23:57:45 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -30,9 +30,27 @@ sed -i 's/enabled=1/enabled=0/' /etc/yum.repos.d/fedora-updates.repo
 cd /tmp
 
 curl -L https://kojipkgs.fedoraproject.org//packages/mesa/24.1.2/7.fc40/x86_64/mesa-libEGL-24.1.2-7.fc40.x86_64.rpm -o mesa-libEGL-24.1.2-7.fc40.x86_64.rpm
-rpm-ostree override replace https://kojipkgs.fedoraproject.org//packages/mesa/24.1.2/7.fc40/x86_64/mesa-libgbm-24.1.2-7.fc40.x86_64.rpm https://kojipkgs.fedoraproject.org//packages/mesa/24.1.2/7.fc40/x86_64/mesa-libglapi-24.1.2-7.fc40.x86_64.rpm
+rpm-ostree override replace https://kojipkgs.fedoraproject.org//packages/mesa/24.1.2/7.fc40/x86_64/mesa-dri-drivers-24.1.2-7.fc40.x86_64.rpm \
+                            https://kojipkgs.fedoraproject.org//packages/mesa/24.1.2/7.fc40/x86_64/mesa-filesystem-24.1.2-7.fc40.x86_64.rpm \
+                            https://kojipkgs.fedoraproject.org//packages/mesa/24.1.2/7.fc40/x86_64/mesa-va-drivers-24.1.2-7.fc40.x86_64.rpm \
+                            https://kojipkgs.fedoraproject.org//packages/mesa/24.1.2/7.fc40/x86_64/mesa-libEGL-24.1.2-7.fc40.x86_64.rpm \
+                            https://kojipkgs.fedoraproject.org//packages/mesa/24.1.2/7.fc40/x86_64/mesa-libGL-24.1.2-7.fc40.x86_64.rpm \
+                            https://kojipkgs.fedoraproject.org//packages/mesa/24.1.2/7.fc40/x86_64/mesa-vulkan-drivers-24.1.2-7.fc40.x86_64.rpm \
+                            https://kojipkgs.fedoraproject.org//packages/mesa/24.1.2/7.fc40/x86_64/mesa-libglapi-24.1.2-7.fc40.x86_64.rpm \
+                            https://kojipkgs.fedoraproject.org//packages/mesa/24.1.2/7.fc40/x86_64/mesa-libgbm-24.1.2-7.fc40.x86_64.rpm \
+                            https://kojipkgs.fedoraproject.org//packages/mesa/24.1.2/7.fc40/i686/mesa-libOSMesa-24.1.2-7.fc40.i686.rpm \
+                            https://kojipkgs.fedoraproject.org//packages/mesa/24.1.2/7.fc40/x86_64/mesa-libglapi-24.1.2-7.fc40.i686.rpm \
+                            https://kojipkgs.fedoraproject.org//packages/mesa/24.1.2/7.fc40/x86_64/mesa-libgbm-24.1.2-7.fc40.i686.rpm \
+                            https://kojipkgs.fedoraproject.org//packages/mesa/24.1.2/7.fc40/x86_64/mesa-dri-drivers-24.1.2-7.fc40.i686.rpm \
+                            https://kojipkgs.fedoraproject.org//packages/mesa/24.1.2/7.fc40/x86_64/mesa-filesystem-24.1.2-7.fc40.i686.rpm \
+                            https://kojipkgs.fedoraproject.org//packages/mesa/24.1.2/7.fc40/x86_64/mesa-va-drivers-24.1.2-7.fc40.i686.rpm \
+                            https://kojipkgs.fedoraproject.org//packages/mesa/24.1.2/7.fc40/x86_64/mesa-libEGL-24.1.2-7.fc40.i686.rpm \
+                            https://kojipkgs.fedoraproject.org//packages/mesa/24.1.2/7.fc40/x86_64/mesa-libGL-24.1.2-7.fc40.i686.rpm \
+                            https://kojipkgs.fedoraproject.org//packages/mesa/24.1.2/7.fc40/x86_64/mesa-vulkan-drivers-24.1.2-7.fc40.i686.rpm \
+                            
+                            
 
-rpm-ostree override replace /tmp/mesa-libEGL-24.1.2-7.fc40.x86_64.rpm
+# rpm-ostree override replace /tmp/mesa-libEGL-24.1.2-7.fc40.x86_64.rpm
 sed -i 's/enabled=0/enabled=1/' /etc/yum.repos.d/fedora.repo
 sed -i 's/enabled=0/enabled=1/' /etc/yum.repos.d/fedora-updates.repo
 
